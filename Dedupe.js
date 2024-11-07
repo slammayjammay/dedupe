70fc64e97b71c609d3d47ebe52cccdb34d3a762e
Scott McKenzie <slammayjammay@gmail.com>
2024-11-06 20:14:57 -0800

    Initial

---
+++ Dedupe.js
@@ -0,0 +1,116 @@
+import util from 'util';
+
+// Simple example: you need to render a video (expensive operation), the same
+// video, in a list showcasing different video dimensions, small medium large
+// etc. If your list involves many videos at the same dimensions, it's better
+// to render 1 video and reuse it rather than rendering X number of videos.
+// Let's also say some sort of state changes and now you have the same number
+// of videos to render, but each video unexpectedly changes to an unknown
+// dimension. This class can be used to render the fewest videos for each state
+// change.
+//
+// Solution for simple example: assign a unique id and a "serial" to each list
+// item, where identical serials between items means the expensive operation
+// can be shared between all items of the same serial. In the video example
+// above, the serial can be the dimensions "400x500". Provide this class an
+// id+serial pair for each item and it will do the rest of the deduping.
+//
+// Call `set()` to add a new id+serial pair or denote a change to an existing
+// one.
+//
+// Call `delete()` to remove an id.
+//
+// `set()` and `delete()` will not immediately change internal state only
+// records changes as todo. Call `cleanup()` to implement the changes.
+//
+// Call `recalculate()` to throw away pending changes and calculate internal
+// state from scratch.
+export default class Dedupe {
+	constructor() {
+		this.map = new Map(); // id -> Node
+		this.trees = new Map(); // serial -> Tree
+		this.changes = []; // { change: 'add|delete', id, serial }
+	}
+
+	createTree(serial) {
+		return { serial, root: null, children: new Map() };
+	}
+
+	createNode(id, serial) {
+		return { id, serial };
+	}
+
+	isRoot(node) {
+		return this.trees.get(node.serial)?.root === node;
+	}
+
+	set(id, serial) {
+		this.changes.push({ change: 'add', id, serial });
+	}
+
+	delete(id) {
+		this.changes.push({ change: 'delete', id });
+	}
+
+	cleanup() {
+		while (this.changes.length) {
+			const { change, id, serial } = this.changes.shift();
+
+			if (change === 'add') {
+				this.addNode(id, serial);
+			} else if (change === 'delete') {
+				this.deleteNode(id);
+			}
+		}
+	}
+
+	addNode(id, serial) {
+		const exists = this.map.has(id);
+		const node = exists ? this.map.get(id) : this.createNode(id, serial);
+
+		// when a serial changes for an id, delete it and add it back in
+		exists && serial !== node.serial && this.changes.unshift(
+			{ change: 'delete', ...node },
+			{ change: 'add', id: node.id, serial }
+		);
+
+		if (exists) return;
+
+		this.map.set(id, node);
+
+		const treeExists = this.trees.has(node.serial);
+		!treeExists && this.trees.set(node.serial, this.createTree(serial));
+
+		const tree = this.trees.get(node.serial);
+		!treeExists ? (tree.root = node) : tree.children.set(node.id, node);
+	}
+
+	deleteNode(id) {
+		if (!this.map.has(id)) return false;
+
+		const node = this.map.get(id);
+		this.map.delete(id);
+
+		if (this.isRoot(node)) {
+			Array.from(this.trees.get(node.serial).children.values()).forEach(node => {
+				this.map.delete(node.id);
+				this.changes.unshift({ change: 'add', ...node });
+			});
+			this.trees.delete(node.serial);
+		} else {
+			this.trees.get(node.serial).children.delete(node.id);
+		}
+	}
+
+	recalculate() {
+		const nodes = Array.from(this.map.values());
+		this.map.clear();
+		this.trees.clear();
+		this.changes.splice(0, this.changes.length, nodes.map(n => ({ change: 'add', ...n })));
+		this.cleanup();
+	}
+
+	destroy() {
+		this.map = this.trees = this.changes = null;
+	}
+}
---
+++ test/index.js
@@ -0,0 +1,114 @@
+import util from 'util';
+import assert from 'assert';
+import Dedupe from '../Dedupe.js';
+
+describe('Dedupe', () => {
+	it(`can do simple dedupes`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(3, 'a');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 1);
+		assert.equal(dedupe.trees.get('a').root.serial, 'a');
+		assert.equal(dedupe.trees.get('a').children.size, 2);
+		assert.equal(dedupe.trees.get('a').children.has(1), false);
+	});
+
+	it(`ignores ids if serial is unchanged`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(2, 'a');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 1);
+		assert.equal(dedupe.trees.get('a').root.id, 1);
+		assert.equal(dedupe.trees.get('a').children.size, 1);
+		assert.equal(dedupe.trees.get('a').children.get(2).id, 2);
+	});
+
+	it(`can create new trees`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('a').root.id, 1);
+		assert.equal(dedupe.trees.get('b').root.id, 2);
+	});
+
+	it(`can swap a child's serial to a new tree`, () => {
+		const dedupe = new Dedupe();
+		for (let i = 0; i < 5; i++) dedupe.set(i, 'a');
+		dedupe.set(1, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('a').root.id, 0);
+		assert.equal(dedupe.trees.get('a').children.size, 3);
+		assert.equal(dedupe.trees.get('b').children.size, 0);
+		assert.equal(dedupe.trees.get('b').root.id, 1);
+	});
+
+	it(`can swap a child's serial to an existing tree`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(3, 'b');
+		dedupe.set(2, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('b').root.id, 3);
+		assert.equal(dedupe.trees.get('b').children.size, 1);
+		assert.equal(dedupe.trees.get('b').children.has(2), true);
+	});
+
+	it(`can swap a root's serial to a new tree`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(1, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('a').root.id, 2);
+		assert.equal(dedupe.trees.get('b').root.id, 1);
+		assert.equal(dedupe.trees.get('a').children.size, 0);
+		assert.equal(dedupe.trees.get('b').children.size, 0);
+	});
+
+	it(`can swap a root's serial to an existing tree`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(3, 'b');
+		dedupe.set(1, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('a').root.id, 2);
+		assert.equal(dedupe.trees.get('b').root.id, 3);
+		assert.equal(dedupe.trees.get('b').children.size, 1);
+		assert.equal(dedupe.trees.get('b').children.has(1), true);
+	});
+
+	it(`can swap a root's serial to an existing tree`, () => {
+		const dedupe = new Dedupe();
+		dedupe.set(1, 'a');
+		dedupe.set(2, 'a');
+		dedupe.set(3, 'b');
+		dedupe.set(1, 'b');
+		dedupe.cleanup();
+
+		assert.equal(dedupe.trees.size, 2);
+		assert.equal(dedupe.trees.get('a').root.id, 2);
+		assert.equal(dedupe.trees.get('b').root.id, 3);
+		assert.equal(dedupe.trees.get('b').children.size, 1);
+		assert.equal(dedupe.trees.get('b').children.has(1), true);
+	});
+});
